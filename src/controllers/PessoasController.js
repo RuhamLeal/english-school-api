352cc0a4a22a33fdbed385f2a261808a963c7d3c
Ruham Leal <ruhamxlpro@hotmail.com>
2022-10-28 13:22:43 -0300

    feat: adding endpoint that restores Person who was deleted

--- src/controllers/PessoasController.js
+++ src/controllers/PessoasController.js
@@ -1,9 +1,9 @@
-const db = require('../models');
+const database = require('../models');
 
 class PessoaController {
   static async getAllPersons(req, res) {
     try {
-      const allPersons = await db.Pessoas.findAll();
+      const allPersons = await database.Pessoas.findAll();
       return res.status(200).json(allPersons);
     } catch (err) {
       return res.status(500).json(err.message);
@@ -13,22 +13,22 @@ class PessoaController {
   static async getPersonById(req, res) {
     const { id } = req.params;
     try {
-      const personById = await db.Pessoas.findOne({
+      const personById = await database.Pessoas.findOne({
         where: { id: Number(id) },
       });
       return res.status(200).json(personById);
     } catch (err) {
-      return res.status(500).send(err.message);
+      return res.status(500).json(err.message);
     }
   }
 
   static async createPerson(req, res) {
     const newPerson = req.body;
     try {
-      const newPersonCreated = await db.Pessoas.create(newPerson);
+      const newPersonCreated = await database.Pessoas.create(newPerson);
       return res.status(200).json(newPersonCreated);
     } catch (err) {
-      return res.status(500).send(err.message);
+      return res.status(500).json(err.message);
     }
   }
 
@@ -36,34 +36,46 @@ class PessoaController {
     const { id } = req.params;
     const newData = req.body;
     try {
-      await db.Pessoas.update(newData, {
+      await database.Pessoas.update(newData, {
         where: { id: Number(id) },
       });
-      const updatedPerson = await db.Pessoas.findOne({
+      const updatedPerson = await database.Pessoas.findOne({
         where: { id: Number(id) },
       });
       return res.status(200).json(updatedPerson);
     } catch (err) {
-      return res.status(500).send(err.message);
+      return res.status(500).json(err.message);
     }
   }
 
   static async deletePerson(req, res) {
     const { id } = req.params;
     try {
-      await db.Pessoas.destroy({
+      await database.Pessoas.destroy({
         where: { id: Number(id) },
       });
       return res.status(200).json({ message: 'Pessoa apagada com sucesso' });
     } catch (err) {
-      return res.status(500).send(err.message);
+      return res.status(500).json(err.message);
+    }
+  }
+
+  static async restorePerson(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Pessoas.restore({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json({ message: `Person with id ${id} restored` });
+    } catch (err) {
+      return res.status(500).json(err.message);
     }
   }
 
   static async getRegisterByPersonId(req, res) {
     const { studentId, registerId } = req.params;
     try {
-      const registerById = await db.Matriculas.findOne({
+      const registerById = await database.Matriculas.findOne({
         where: { 
           id: Number(registerId),
           estudante_id: Number(studentId),
@@ -71,7 +83,7 @@ class PessoaController {
       });
       return res.status(200).json(registerById);
     } catch (err) {
-      return res.status(500).send(err.message);
+      return res.status(500).json(err.message);
     }
   }
 
@@ -79,10 +91,10 @@ class PessoaController {
     const { studentId } = req.params;
     const newRegister = { ...req.body, estudante_id: Number(studentId) };
     try {
-      const newRegisterCreated = await db.Matriculas.create(newRegister);
+      const newRegisterCreated = await database.Matriculas.create(newRegister);
       return res.status(200).json(newRegisterCreated);
     } catch (err) {
-      return res.status(500).send(err.message);
+      return res.status(500).json(err.message);
     }
   }
 
@@ -90,30 +102,30 @@ class PessoaController {
     const { studentId, registerId } = req.params;
     const newData = req.body;
     try {
-      await db.Matriculas.update(newData, {
+      await database.Matriculas.update(newData, {
         where: { 
           id: Number(registerId),
           estudante_id: Number(studentId),
         },
       });
-      const updatedRegister = await db.Matriculas.findOne({
+      const updatedRegister = await database.Matriculas.findOne({
         where: { id: Number(registerId) },
       });
       return res.status(200).json(updatedRegister);
     } catch (err) {
-      return res.status(500).send(err.message);
+      return res.status(500).json(err.message);
     }
   }
 
   static async deleteRegister(req, res) {
     const { registerId } = req.params;
     try {
-      await db.Matriculas.destroy({
+      await database.Matriculas.destroy({
         where: { id: Number(registerId) },
       });
       return res.status(200).json({ message: 'Matricula apagada com sucesso' });
     } catch (err) {
-      return res.status(500).send(err.message);
+      return res.status(500).json(err.message);
     }
   }
 }
--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -7,6 +7,7 @@ const routeToRegisterByID = '/pessoas/:studentId/matricula/:registerId';
 const routeToPersonByID = '/pessoas/:id';
 const routeToRegisterByPersonID = '/pessoas/:studentId/matricula';
 const routeToPerson = '/pessoas';
+const routeToRestorePerson = '/pessoas/:id/restore';
 
 router
   .get(routeToPerson, PessoaController.getAllPersons)
@@ -14,6 +15,7 @@ router
   .get(routeToRegisterByID, PessoaController.getRegisterByPersonId)
   .post(routeToPerson, PessoaController.createPerson)
   .post(routeToRegisterByPersonID, PessoaController.createRegisterByPersonId)
+  .post(routeToRestorePerson, PessoaController.restorePerson)
   .put(routeToPersonByID, PessoaController.updatePerson)
   .put(routeToRegisterByID, PessoaController.updateRegister)
   .delete(routeToPersonByID, PessoaController.deletePerson)
